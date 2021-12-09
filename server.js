975586b3576b79fd21fc497ee14ccaada7cadea0
sanghmitrav <sanghmitra.v@codeparva.in>
2021-12-09 17:12:41 +0530

    removed console logs

--- server.js
+++ server.js
@@ -8,14 +8,9 @@ app.use(express.json());
 app.use(require("./routes/record"));
 // get driver connection
 const dbo = require("./db/conn");
-
-console.log("entering in listen");
 app.listen(port, () => {
-  console.log("entered in listen");
   // perform a database connection when server starts
   dbo.connectToServer(function (err) {
-    console.log("connecting to server");
-
     if (err) console.error(err);
   });
   console.log(`Server is running on port: ${port}`);
